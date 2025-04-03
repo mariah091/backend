b1c722598a102e608982ab450cfb54bbd7c2f946
mariah091 <mariahvitoriamartinsvha@gmail.com>
2025-04-03 11:42:55 -0400

    exercicios

---
+++ exercicios/exercios.js
@@ -0,0 +1,20 @@
+// 1º exercicio
+let  nome = "Mariah";
+let idade = '15';
+console.log("Olá, meu nome é" ,nome,  "eu tenho" ,idade,  "anos");
+
+// 2º exercicio
+let Olá = 'Olá';
+
+console.log(Olá.toLowerCase())
+console.log(Olá.toUpperCase());
+ 
+// 3º exercicio
+let nome2 = 'Mariah'
+console.log(typeof nome2)
+
+//4º exercicio
+let peso = 56
+let altura = 1.69
+let soma = peso/(altura*altura)
+console.log(soma)
