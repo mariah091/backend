7b4c81d3f269d5dcbca53dbea647967a886358c7
mariah091 <mariahvitoriamartinsvha@gmail.com>
2025-04-10 10:07:41 -0400

    Atividades 01

--- exercicios/exercios.js
+++ exercicios/exercios.js
@@ -1,7 +1,7 @@
 // 1º exercicio
 let  nome = "Mariah";
 let idade = '15';
-console.log("Olá, meu nome é" ,nome,  "eu tenho" ,idade,  "anos");
+console.log("Olá, meu nome é " + nome + "e eu tenho" + idade + "anos");
 
 // 2º exercicio
 let Olá = 'Olá';
@@ -16,5 +16,5 @@ console.log(typeof nome2)
 //4º exercicio
 let peso = 56
 let altura = 1.69
-let soma = peso/(altura*altura)
-console.log(soma)
+let soma = (peso/(altura*altura)).toFixed(2);
+console.log(`Seu IMC é: ${soma}`);
