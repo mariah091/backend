f50720af0dace697f89b0e1f8fa0c0c2768499e6
mariah091 <mariahvitoriamartinsvha@gmail.com>
2025-04-24 10:13:16 -0400

    correcao

--- exercicios/exercicio-02.js
+++ exercicios/exercicio-02.js
@@ -3,17 +3,26 @@
 
 let  vlr1 = 15
 let  vlr2 = 20
-console.log("soma = ",vlr1 + vlr2)
-console.log("subtração =" ,vlr1 - vlr2)
-console.log("multiplicação = ",vlr1 * vlr2)
-console.log("divisão = " ,vlr1 / vlr2);
-
+//console.log("soma = ",vlr1 + vlr2)
+//console.log("subtração =" ,vlr1 - vlr2)
+//console.log("multiplicação = ",vlr1 * vlr2)
+//console.log("divisão = " ,vlr1 / vlr2);
+let soma = vlr1 + vlr2
+let subtração = vlr1 - vlr2
+let multiplicação = vlr1 * vlr2
+let divisão = vlr1 / vlr2
+console.log(`
+    ${vlr1} + ${vlr2} = ${soma}
+    ${vlr1} - ${vlr2} = ${subtração}
+    ${vlr1} * ${vlr2} = ${multiplicação}
+    ${vlr1} / ${vlr2} = ${divisão} `);
+    
 //2. Declare duas idades e utilize operadores de comparação para verificar se uma
 //pessoa é mais velha que a outra.
-let  idd1 = 28
-let  idd2 = 56
-console.log(idd1 == idd2);
-
+let  idda1 = 28
+let  idda2 = 15
+let resultado = idda1 < idda2 ? 'O segundo é mais velho' : 'O primeiro é mais velho'
+console.log(resultado);
 //3. Crie uma variável idade com um número e use o operador ternário para verificar
 //se o valor armazenado na variável é maior ou igual a 18. Imprima “Maior de
 //idade” ou “Menor de idade”.
@@ -24,5 +33,5 @@ console.log (mensagem);
 //4. Crie uma variável com um número e use o operador ternário para verificar se é
 //par ou ímpar.
 let num = 10;
-let resto = num % 2 == 0;
-console.log(resto)
+let resto = num % 2 == 0 ? "Par" : "Impar"
+console.log(resto);
