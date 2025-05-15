cfcb356a99261f7cfeb53be585e9752ad5ab03e7
mariah091 <mariahvitoriamartinsvha@gmail.com>
2025-05-15 10:45:04 -0400

    .

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,24 @@
 //oi
 //olá
 console.log("Hello, World");
+
+let valor = 10
+let outrovalor = valor // passagem como valor -> passa uma cópia do valor
+console.log("Valor: " + valor)
+console.log("Outro valor:" + outrovalor)
+outrovalor = 15
+console.log("Valor:" + valor)
+console.log("Outro valor: " + outrovalor);
+
+let carro = {
+    cor : "Azul"
+}
+
+let bicicleta = carro 
+
+console.log ("Carro: " + carro.cor)
+console.log ("Bicicleta: " + bicicleta.cor)
+
+bicicleta.cor = "Verde"
+console.log("Carro: " + carro.cor)
+console.log("Bicicleta: " + bicicleta.cor)
